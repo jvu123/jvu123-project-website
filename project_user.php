6df61b315395ee1a39b430a79ef4ffafe9cd43dc
jvu123 <37727359+jvu123@users.noreply.github.com>
2022-04-11 22:54:11 -0400

    fixing navbar

--- project_user.php
+++ project_user.php
@@ -1,15 +1,19 @@
-<?php
-
-
-
-?>
-
 <!DOCTYPE html>
 <html lang="en">
-
-<!--navbar content-->
-    
-<?php include('templates/navbar.php'); ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Database</title>
+    <link rel="stylesheet" href="project_style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <script src="https://kit.fontawesome.com/d791d40d5f.js" crossorigin="anonymous"></script>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
+</head>
+<body>
+    <section class="header">
+        <?php include('templates/navbar.php'); ?>
 
     <div class="intro">
         <h1>User Profile</h1>
