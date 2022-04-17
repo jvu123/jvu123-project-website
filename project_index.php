9378e70cdf83c4714b7798dd587cbcb8c03fe722
Johnson Vu <johnsonvu123@gmail.com>
2022-04-17 10:33:42 -0400

    replaced head of pages with php include

--- project_index.php
+++ project_index.php
@@ -2,19 +2,7 @@
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project Database</title>
-    <link rel="stylesheet" href="project_style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <script src="https://kit.fontawesome.com/d791d40d5f.js" crossorigin="anonymous"></script>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
-</head>
+<?php include('templates/head.php'); ?>
 <body>
     <section class="header">
         <?php include('templates/navbar.php'); ?>
--- project_login.php
+++ project_login.php
@@ -35,19 +35,8 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Project Database</title>
-    <link rel="stylesheet" href="project_style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <script src="https://kit.fontawesome.com/d791d40d5f.js" crossorigin="anonymous"></script>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
-</head>
+<?php include('templates/head.php'); ?>
+
 <body>
     <section class="header">
         <?php include('templates/navbar.php'); ?>
@@ -62,7 +51,8 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
                     <div class="form_input">
                         <input type="password" name="password" placeholder="Enter your password"/>
                     </div>
-                    <input type="submit" name="submit" value="LOGIN" class="btn-login"/>
+                    <input type="submit" name="submit" value="LOGIN" class="btn-login"/><br><br>
+                    <a href="signup.php">Click to Signup</a>
                 </form>
             </div>
         </div>
