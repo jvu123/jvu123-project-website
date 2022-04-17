b02297203fc034805a206cb37c25128651ac8f8b
Johnson Vu <johnsonvu123@gmail.com>
2022-04-17 10:39:08 -0400

    undid include

--- project_login.php
+++ project_login.php
@@ -35,8 +35,19 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
 ?>
 
-<?php include('templates/head.php'); ?>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Database</title>
+    <link rel="stylesheet" href="project_style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <script src="https://kit.fontawesome.com/d791d40d5f.js" crossorigin="anonymous"></script>
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@200;300;400;600;700&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/fontawesome.min.css">
+</head>
 <body>
     <section class="header">
         <?php include('templates/navbar.php'); ?>
@@ -52,7 +63,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
                         <input type="password" name="password" placeholder="Enter your password"/>
                     </div>
                     <input type="submit" name="submit" value="LOGIN" class="btn-login"/><br><br>
-                    <a href="signup.php">Click to Signup</a>
+                    <a href="project_signup.php">Click to Signup</a>
                 </form>
             </div>
         </div>
