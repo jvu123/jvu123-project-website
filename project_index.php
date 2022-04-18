a250ce3ebcebe0f5ab387b317302dc08bd138bf4
jvu123 <37727359+jvu123@users.noreply.github.com>
2022-04-18 17:02:42 -0400

    php files for scripts

--- project_index.php
+++ project_index.php
@@ -26,79 +26,15 @@
 
     </section>
 
-    <!-- Location section -->
-
-    <section class="location">
-        <h1>Location</h1>
-        <p>Choose a location</p>
-
-        <div class="row">
-            <div class="location-col">
-                <img src="images/village.jpg">
-                <div class="layer">
-                    <h3>Village</h3>
-                    <a href="project_village.php"></a>
-                </div>
-            </div>
-            <div class="location-col">
-                <img src="images/town.jpg">
-                <div class="layer">
-                    <h3>Town</h3>
-                    <a href="project_town.php"></a>
-                </div>
-            </div>
-            <div class="location-col">
-                <img src="images/city.jpg">
-                <div class="layer">
-                    <h3>City</h3>
-                    <a href="project_city.php"></a>
-                </div>
-            </div>
-            <div class="location-col">
-                <img src="images/metro.jpg">
-                <div class="layer">
-                    <h3>Metropolis</h3>
-                    <a href="project_metro.php"></a>
-                </div>
-            </div>
-        </div>
-    </section>
-
     <!-- Footer -->
 
     <?php include('templates/footer.php'); ?>
 
-<!-------JavaScript for Toggle Menu ----->
-<script>
-    var navLinks = document.getElementById("navLinks");
-    function showMenu(){
-        navLinks.style.right = "0";
-    }
-    function hideMenu(){
-        navLinks.style.right = "-200px";
-    }
-</script>
+    <!-- Javascript -->
 
-<!---JavaScript for Sticky Nav Bar-->
-<script>
-    // When the user scrolls the page, execute myFunction
-    window.onscroll = function() {myFunction()};
+    <?php include('scripts/nav_sticky.php'); ?>
 
-    // Get the navbar
-    var navbar = document.getElementById("navbar");
-
-    // Get the offset position of the navbar
-    var sticky = navbar.offsetTop;
-
-    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
-</script>
+    <?php include('scripts/toggle_menu.php'); ?>
 
 
 </body>
--- project_signup.php
+++ project_signup.php
@@ -66,37 +66,11 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 
     <?php include('templates/footer.php'); ?>
 
-<!-------JavaScript for Toggle Menu ----->
-<script>
-    var navLinks = document.getElementById("navLinks");
-    function showMenu(){
-        navLinks.style.right = "0";
-    }
-    function hideMenu(){
-        navLinks.style.right = "-200px";
-    }
-</script>
+    <!-- Javascript -->
 
-<!---JavaScript for Sticky Nav Bar-->
-<script>
-    // When the user scrolls the page, execute myFunction
-    window.onscroll = function() {myFunction()};
+    <?php include('scripts/nav_sticky.php'); ?>
 
-    // Get the navbar
-    var navbar = document.getElementById("navbar");
-
-    // Get the offset position of the navbar
-    var sticky = navbar.offsetTop;
-
-    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function myFunction() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
-</script>
+    <?php include('scripts/toggle_menu.php'); ?>
 
 
 </body>
